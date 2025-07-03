d6bd8825b14ec46976b5a9815138935e4b35e771
Dhany Jogchand <jayjogchand50@gmail.com>
2025-07-03 16:58:40 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.secret_key = "Dhany2005"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:Dhany%402005@localhost/gs_travels"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    contact_no = db.Column(db.String(20), nullable=False)
+    pickup = db.Column(db.String(100), nullable=False)
+    destination = db.Column(db.String(100), nullable=False)
+    booking_time = db.Column(db.DateTime, default=datetime.utcnow)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        # Extract form data
+        name = request.form.get('Name')
+       
+        contact_no = request.form.get('contactno')
+        pickup = request.form.get('Pickup')
+        destination = request.form.get('Destiny')
+        # You can add more validation here if needed
+
+        # Create new booking
+        booking = Booking(
+            name=name,
+            contact_no=contact_no,
+            pickup=pickup,
+            destination=destination
+        )
+        db.session.add(booking)
+        db.session.commit()
+
+    allbooking = Booking.query.all()
+    return render_template("index.html", allbooking=allbooking)
+
+if __name__ == "__main__":
+    app.run()
